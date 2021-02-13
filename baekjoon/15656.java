584b426cf8bd80a5801e0857897fe67873c26d26
LeeHyeSu <hsy4896@daum.net>
2021-02-13 12:01:35 +0900

    [Backtracking] N과 M (7)

---
+++ baekjoon/15656.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n, m;
+	static int[] arr, pick;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new int[n];
+		pick = new int[m];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		comb(0);
+		
+		System.out.println(sb);
+	}
+	
+	public static void comb(int count) {
+		if (count == m) {
+			for (int value : pick) {
+				sb.append(value).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			pick[count] = arr[i];
+			comb(count + 1);
+		}
+	}
+
+}  
