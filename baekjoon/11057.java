c377612078991461a3800ec32653b6dd5b2b2a3a
LeeHyeSu <hsy4896@daum.net>
2021-03-13 23:02:43 +0900

    [DP] 오르막 수

---
+++ baekjoon/11057.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] dp = new int[n + 1][10];
+		
+		for (int i = 0; i < 10; i++) {
+			dp[1][i] = 1;
+		}
+		
+		// dp[n][j] = dp[n-1][0] + dp[n-1][1] + ... + dp[n-1][j];
+		for (int i = 2; i <= n; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k <= j; k++) {
+					dp[i][j] += dp[i - 1][k];
+				}
+				dp[i][j] %= 10007;
+			}
+		}
+		
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += dp[n][i];
+		}
+		
+		System.out.println(sum % 10007);
+	}
+
+ }
